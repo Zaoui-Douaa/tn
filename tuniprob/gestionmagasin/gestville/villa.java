49eed5489aed9f23a8357b88410de0acb11c6827
nour <nourbelhediwork@gmail.com>
2023-10-28 16:02:29 +0100

    2nd commit

--- tuniprob/gestionmagasin/gestville/villa.java
+++ tuniprob/gestionmagasin/gestville/villa.java
@@ -1,2 +1,35 @@
-package tn.tuniprob.gestionmagasin.gestville;public class villa {
+package tn.tuniprob.gestionmagasin.gestville;
+
+public class villa {
+    private int id ;
+    private String nom;
+
+    public villa(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "villa{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
 }
